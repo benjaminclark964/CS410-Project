d68b4e8e9938123692d844738a257f32333a6f08
Benjamin <benjaminclark964@u.boisestate.edu>
2020-04-12 09:18:45 -0600

    init

---
+++ src/main/java/main/java/edu/boisestate/cs410/gradebook/GradeBookShell/GradeBookShell.java
@@ -0,0 +1,10 @@
+package main.java.edu.boisestate.cs410.gradebook.GradeBookShell;
+
+import com.budhash.cliche.Command;
+import com.budhash.cliche.ShellFactory;
+
+public class GradeBookShell {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
