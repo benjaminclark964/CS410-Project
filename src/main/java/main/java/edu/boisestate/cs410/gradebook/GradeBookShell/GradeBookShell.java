539460b57d88642912e4c7dfad2b30efa7ed54a2
Benjamin <benjaminclark964@u.boisestate.edu>
2020-04-12 14:21:44 -0600

    added database connection code

--- src/main/java/main/java/edu/boisestate/cs410/gradebook/GradeBookShell/GradeBookShell.java
+++ src/main/java/main/java/edu/boisestate/cs410/gradebook/GradeBookShell/GradeBookShell.java
@@ -3,8 +3,26 @@ package main.java.edu.boisestate.cs410.gradebook.GradeBookShell;
 import com.budhash.cliche.Command;
 import com.budhash.cliche.ShellFactory;
 
+import java.io.IOException;
+import java.sql.*;
+
 public class GradeBookShell {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    private final Connection dbURL;
+
+    public GradeBookShell(Connection connect) {
+        dbURL = connect;
+    }
+
+    public static void main(String[] args) throws SQLException, IOException {
+        String dbURL = args[0];
+        System.out.println(dbURL);
+        try(Connection connect = DriverManager.getConnection("jdbc:" + dbURL)) {
+            GradeBookShell shell = new GradeBookShell(connect);
+            ShellFactory.createConsoleShell("grade-manager", "", shell).commandLoop();
+            System.out.println("Connection Successful");
+        } catch(Exception e) {
+            System.err.println("Error: Failed to connect");
+            System.err.println(e.toString());
+        }
     }
 }
