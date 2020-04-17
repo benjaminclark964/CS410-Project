0c0041ee7c139ca0f15a5d0e080769352ec15260
Benjamin <benjaminclark964@u.boisestate.edu>
2020-04-17 15:08:42 -0600

    Implemented select class

--- src/main/java/edu/boisestate/cs410/gradebook/GradeBookShell/GradeBookShell.java
+++ src/main/java/edu/boisestate/cs410/gradebook/GradeBookShell/GradeBookShell.java
@@ -13,11 +13,108 @@ public class GradeBookShell {
         db = connect;
     }
 
+    //Class Management----------------------------------------------------------------
+
     @Command
     public void newClass(String courseNumber, String semester, String sectionNumber,
                          String description) {
 
     }
+
+    @Command
+    public void listClasses() throws SQLException {
+        String query = "SELECT course_number as class, COUNT(Student.class_id) as num_students FROM Class\n" +
+                "JOIN Student USING (class_id)\n" +
+                "GROUP BY course_number, class_id;";
+        try(Statement stmt = db.createStatement();
+            ResultSet rs = stmt.executeQuery(query)) {
+            while(rs.next()) {
+                System.out.format("%s | %s%n",
+                        rs.getString(1),
+                        rs.getString(2));
+            }
+        }
+    }
+
+    @Command
+    public void selectClass(String courseNumber) throws SQLException {
+        String query = "SELECT * FROM Class\n" +
+                "WHERE course_number=? AND term='SP20';";
+        try(PreparedStatement stmt = db.prepareStatement(query)) {
+            stmt.setString(1, courseNumber);
+            try(ResultSet rs = stmt.executeQuery()) {
+                int numSections = 0;
+                while(rs.next()) {
+                    numSections++;
+                    if(numSections < 2) {
+                        System.out.format("%d %s %s %s %d %s%n",
+                                rs.getInt(1),
+                                rs.getString(2),
+                                rs.getString(3),
+                                rs.getString(4),
+                                rs.getInt(5),
+                                rs.getString(6));
+                    } else {
+                        System.err.println("ERROR: Only one section should exist");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    @Command
+    public void selectClass(String courseNumber, String term) throws SQLException {
+        String query = "SELECT * FROM Class\n" +
+                "WHERE course_number=? AND term=?;";
+        try(PreparedStatement stmt = db.prepareStatement(query)) {
+            stmt.setString(1, courseNumber);
+            stmt.setString(2, term);
+            try(ResultSet rs = stmt.executeQuery()) {
+                int numSections = 0;
+                while(rs.next()) {
+                    numSections++;
+                    if(numSections < 2) {
+                        System.out.format("%d %s %s %s %d %s%n",
+                                rs.getInt(1),
+                                rs.getString(2),
+                                rs.getString(3),
+                                rs.getString(4),
+                                rs.getInt(5),
+                                rs.getString(6));
+                    } else {
+                        System.err.println("ERROR: Only one section should exist");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    @Command
+    public void selectClass(String courseNumber, String term, String section) throws SQLException {
+        String query = "SELECT * FROM Class\n" +
+                "WHERE course_number=? AND term=? AND section_number=?;";
+        try(PreparedStatement stmt = db.prepareStatement(query)) {
+            stmt.setString(1, courseNumber);
+            stmt.setString(2, term);
+            stmt.setString(3, section);
+
+            try(ResultSet rs = stmt.executeQuery()) {
+                if(rs.next()) {
+                    System.out.format("%d %s %s %s %d %s%n",
+                            rs.getInt(1),
+                            rs.getString(2),
+                            rs.getString(3),
+                            rs.getString(4),
+                            rs.getInt(5),
+                            rs.getString(6));
+                }
+            }
+
+        }
+    }
+
 
     @Command
     public void showClass() throws SQLException {
@@ -38,6 +135,8 @@ public class GradeBookShell {
         }
     }
 
+    //Category and Item Management-----------------------------------------------------
+
     @Command
     public void showCategories() throws SQLException {
         String query = "SELECT * FROM Categories;";
@@ -56,10 +155,10 @@ public class GradeBookShell {
     public void showItems() throws SQLException {
         String query = "SELECT itemname, point_value FROM Items\n" +
                 "GROUP BY id, category_name;";
-        try(Statement stmt = db.createStatement();
-        ResultSet rs = stmt.executeQuery(query)) {
+        try (Statement stmt = db.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
 
-            while(rs.next()) {
+            while (rs.next()) {
                 System.out.format("%s | %s%n",
                         rs.getString(1),
                         rs.getString(2));
@@ -67,20 +166,11 @@ public class GradeBookShell {
         }
     }
 
-    @Command
-    public void listClasses() throws SQLException {
-        String query = "SELECT course_number as class, COUNT(Student.class_id) as num_students FROM Class\n" +
-                "JOIN Student USING (class_id)\n" +
-                "GROUP BY course_number, class_id;";
-        try(Statement stmt = db.createStatement();
-        ResultSet rs = stmt.executeQuery(query)) {
-            while(rs.next()) {
-                System.out.format("%s | %s%n",
-                        rs.getString(1),
-                        rs.getString(2));
-            }
-        }
-    }
+    //Student Management--------------------------------------------------------------
+
+
+    //Grade Reporting-----------------------------------------------------------------
+
 
     public static void main(String[] args) throws SQLException, IOException {
         String dbURL = args[0];
